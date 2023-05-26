e66cef7e247622e5224d63ea966b6c3787c5ea1c
leej1230 <leejaewoo1230@gmail.com>
2023-05-26 02:52:41 -0700

    implemented todo models and inserted objects manually using ruby console. Then displayed on web app.

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,6 @@
 class TodosController < ApplicationController
   def index
+    # Instance variable
+    @todos = Todo.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
 
+  root "todos#index"
   get "/todos", to: "todos#index"
 end
