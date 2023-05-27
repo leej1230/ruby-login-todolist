d90cdba4be4b7b7eb25d0b65257b90c76e214ca9
leej1230 <leejaewoo1230@gmail.com>
2023-05-27 03:18:28 -0700

    registration feature done

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,4 +1,22 @@
 class UserController < ApplicationController
   def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      # Handle successful user registration
+      redirect_to root_url, notice: 'User registered successfully!'
+    else
+      # Handle failed user registration
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   get "/login", to: "sessions#new"
-  get "/signup" to: "users#new"
+  get "/signup", to: "user#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
 
   root "todos#index"
   resources :todos
+  resources :users, only: [:new, :create]
 end
